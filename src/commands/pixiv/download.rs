c7b2722d42c9e04db485ed62b3f54a6b51838553
WOo0W <24245520+WOo0W@users.noreply.github.com>
2021-12-13 22:50:20 +0800

    refactor pixiv commands

---
+++ src/commands/pixiv/download.rs
@@ -0,0 +1,290 @@
+use futures::FutureExt;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use pixivcrab::AppAPI;
+
+use std::{collections::HashMap, path::PathBuf};
+use tokio::task::spawn_blocking;
+
+use reqwest::{Method, Url};
+use snafu::ResultExt;
+
+use crate::{
+    downloader::{ClosureFuture, Downloader, Task, TaskHooks, TaskOptions},
+    error,
+    log::warning,
+};
+
+use mongodb::{
+    bson::{doc, Document},
+    Collection,
+};
+
+use path_slash::PathBufExt;
+
+use super::utils;
+lazy_static! {
+    /// Match the pximg URL.
+    ///
+    /// # Example
+    ///
+    /// Matching the URL
+    /// `https://i.pximg.net/img-original/img/2021/08/22/22/03/33/92187206_p0.jpg`
+    ///
+    /// Groups:
+    ///
+    /// __0__ `/2021/08/22/22/03/33/92187206_p0.jpg`
+    ///
+    /// __1__ `2021/08/22/22/03/33`
+    ///
+    /// __2__ `92187206_p0.jpg`
+    ///
+    /// __3__ `92187206_p0`
+    ///
+    /// __4__ `jpg`
+    static ref RE_ILLUST_URL: Regex =
+        Regex::new(r"/(\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2})/((.*)\.(.*))$").unwrap();
+}
+
+macro_rules! try_skip {
+    ($res:expr) => {
+        match $res {
+            Ok(val) => val,
+            Err(e) => {
+                warning!("{}", e);
+                continue;
+            }
+        }
+    };
+}
+
+fn task_from_illust(
+    api: &AppAPI,
+    c_image: Collection<Document>,
+    raw_url: Option<String>,
+    parent_dir: &PathBuf,
+    user_id: &str,
+    illust_id: &str,
+    is_multi_page: bool,
+    ffmpeg_path: &Option<PathBuf>,
+    ugoira_frame_delay: Option<Vec<i32>>,
+) -> crate::Result<Task> {
+    let url = match raw_url {
+        Some(raw_url) => match raw_url.parse::<Url>() {
+            Ok(url) => url,
+            Err(e) => {
+                return Err(e).context(error::PixivParseURL);
+            }
+        },
+        None => {
+            return error::PixivParse {
+                message: "empty url".to_string(),
+            }
+            .fail()
+        }
+    };
+
+    let captures = RE_ILLUST_URL.captures(url.path()).ok_or(
+        error::PixivParse {
+            message: format!("cannot match url with RE_ILLUST_URL: {}", url),
+        }
+        .build(),
+    )?;
+    let date = captures.get(1).unwrap().as_str().replace("/", "");
+
+    let request_builder = {
+        let url = url.clone();
+        let hash_secret = api.hash_secret.clone();
+        move |client: &reqwest::Client| {
+            client
+                .request(Method::GET, url.clone())
+                .headers(pixivcrab::default_headers(&hash_secret))
+                .header("Referer", "https://app-api.pixiv.net/")
+                .build()
+                .context(error::DownloadRequestBuild)
+        }
+    };
+    let path_slash = if is_multi_page {
+        format!(
+            "{}/{}_{}/{}",
+            user_id,
+            illust_id,
+            date,
+            captures.get(2).unwrap().as_str()
+        )
+    } else {
+        format!(
+            "{}/{}_{}.{}",
+            user_id,
+            captures.get(3).unwrap().as_str(), // filename with page id
+            date,
+            captures.get(4).unwrap().as_str(), // extension
+        )
+    };
+
+    let path_slash_cloned = path_slash.clone();
+
+    let on_success_hook: ClosureFuture = if let Some(ffmpeg_path) = ffmpeg_path {
+        // The task is an ugoira zip.
+        let ffmpeg_path = ffmpeg_path.clone();
+        let zip_url = url.clone();
+        Box::new(move |t| {
+            let zip_size = t.file_size.unwrap().try_into().unwrap_or_default();
+            let zip_path = t.options.path.clone().unwrap();
+
+            async move {
+                let zip_path_cloned = zip_path.clone();
+                spawn_blocking(move || {
+                    utils::ugoira2mp4(&ffmpeg_path, &zip_path_cloned, ugoira_frame_delay.unwrap())
+                })
+                .await
+                .unwrap()?;
+
+                super::database::save_image_ugoira(
+                    &c_image,
+                    zip_url.to_string(),
+                    zip_path,
+                    path_slash,
+                    zip_size,
+                )
+                .await?;
+
+                Ok(())
+            }
+            .boxed()
+        })
+    } else {
+        Box::new(move |t| {
+            let image_path = t.options.path.clone().unwrap();
+            let size = t.file_size.unwrap().try_into().unwrap_or_default();
+            let url = t.url.clone();
+
+            async move {
+                let image_path_cloned = image_path.clone();
+                let ((w, h), rgb_v) =
+                    spawn_blocking(move || utils::get_palette(&image_path_cloned))
+                        .await
+                        .unwrap()?;
+                super::database::save_image(
+                    &c_image,
+                    size,
+                    (w, h),
+                    rgb_v,
+                    url.to_string(),
+                    path_slash,
+                    image_path,
+                )
+                .await?;
+
+                Ok(())
+            }
+            .boxed()
+        })
+    };
+
+    let path = parent_dir.join(PathBuf::from_slash(&path_slash_cloned));
+
+    Ok(Task::new(
+        Box::new(request_builder),
+        url,
+        TaskOptions {
+            path: Some(path),
+            ..Default::default()
+        },
+        Some(TaskHooks {
+            on_error: None,
+            on_success: Some(on_success_hook),
+        }),
+    ))
+}
+
+pub async fn download_illusts(
+    illusts: &Vec<pixivcrab::models::illust::Illust>,
+    ugoira_map: &mut HashMap<String, (String, Vec<i32>)>,
+    api: &AppAPI,
+    downloader: &Downloader,
+    c_image: &Collection<Document>,
+    items_sent: &mut u32,
+    limit: Option<u32>,
+    parent_dir: &PathBuf,
+    ffmpeg_path: &Option<PathBuf>,
+) {
+    let mut tasks = Vec::new();
+    for i in illusts {
+        if let Some(limit) = limit {
+            if *items_sent >= limit {
+                downloader.send(tasks).await;
+                return;
+            }
+        }
+        *items_sent += 1;
+
+        if !i.visible {
+            continue;
+        }
+        let illust_id = i.id.to_string();
+        let is_ugoira = i.r#type == "ugoira";
+
+        if is_ugoira {
+            if let Some((zip_url, delay)) = ugoira_map.remove(&illust_id) {
+                let zip_url = zip_url.replace("600x600", "1920x1080");
+                match task_from_illust(
+                    &api,
+                    c_image.clone(),
+                    // get higher resolution images
+                    Some(zip_url.clone()),
+                    parent_dir,
+                    &i.user.id.to_string(),
+                    &illust_id,
+                    true,
+                    ffmpeg_path,
+                    Some(delay),
+                ) {
+                    Ok(task) => {
+                        tasks.push(task);
+                    }
+                    Err(err) => {
+                        warning!("Fail to build task from {}: {}", zip_url, err)
+                    }
+                }
+            }
+        }
+
+        if i.page_count == 1 {
+            let task = try_skip!(task_from_illust(
+                &api,
+                c_image.clone(),
+                i.meta_single_page.original_image_url.clone(),
+                parent_dir,
+                &i.user.id.to_string(),
+                &illust_id,
+                is_ugoira,
+                &None,
+                None
+            ));
+            tasks.push(task);
+        } else {
+            for img in &i.meta_pages {
+                let task = try_skip!(task_from_illust(
+                    &api,
+                    c_image.clone(),
+                    img.image_urls.original.clone(),
+                    parent_dir,
+                    &i.user.id.to_string(),
+                    &illust_id,
+                    true,
+                    &None,
+                    None
+                ));
+                tasks.push(task);
+            }
+        }
+    }
+    downloader.send(tasks).await;
+    if let Some(limit) = limit {
+        if *items_sent >= limit {
+            return;
+        }
+    }
+}
---
+++ src/commands/pixiv/utils.rs
@@ -0,0 +1,123 @@
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+    process::{Command, Stdio},
+};
+
+use image::GenericImageView;
+use pixivcrab::Pager;
+use rocket::serde::DeserializeOwned;
+use snafu::ResultExt;
+
+use crate::{downloader::BoxError, error, log::warning, models};
+
+pub fn ugoira2mp4<P1: AsRef<Path>, P2: AsRef<Path>>(
+    ffmpeg_path: P1,
+    zip_path: P2,
+    frame_delay: Vec<i32>,
+) -> Result<PathBuf, BoxError> {
+    let zip_path = zip_path.as_ref();
+    let mut mp4_path = PathBuf::from(zip_path);
+
+    let mut file = File::open(zip_path)?;
+    let mut zip_file = zip::ZipArchive::new(&mut file)?;
+    mp4_path.set_extension("mp4");
+
+    let mut ffmpeg = Command::new(ffmpeg_path.as_ref())
+        .args([
+            "-y",
+            "-hide_banner",
+            "-loglevel",
+            "error",
+            "-f",
+            "image2pipe",
+            "-framerate",
+            "60",
+            "-i",
+            "-",
+            "-c:v",
+            "libx264",
+            "-preset",
+            "slow",
+            "-crf",
+            "22",
+            "-pix_fmt",
+            "yuv420p",
+            "-vf",
+            "pad=ceil(iw/2)*2:ceil(ih/2)*2",
+        ])
+        .arg(mp4_path.as_os_str())
+        .stdin(Stdio::piped())
+        .spawn()?;
+    let mut stdin = ffmpeg.stdin.take().unwrap();
+
+    let mut t: f32 = 0.0;
+    let mut frame = 0;
+    for i in 0..zip_file.len() {
+        t += frame_delay
+            .get(i)
+            .ok_or(format!(
+                "Cannot get ugoira frame {} from {:?}",
+                i, frame_delay
+            ))?
+            .clone() as f32;
+        let next_frame = (t / (1000.0 / 60.0)).round() as i32;
+        for _ in frame..next_frame {
+            let mut file = zip_file.by_index(i)?;
+            std::io::copy(&mut file, &mut stdin)?;
+        }
+        frame = next_frame;
+    }
+    drop(stdin);
+    let status = ffmpeg.wait()?;
+    if !status.success() {
+        Err(format!("FFmpeg exited with status {}", status))?
+    }
+    Ok(mp4_path)
+}
+
+pub fn get_palette<P: AsRef<Path>>(
+    image_path: P,
+) -> Result<((i32, i32), Vec<models::RGB>), BoxError> {
+    let img = image::open(image_path)?;
+    let (w, h) = img.dimensions();
+    let thumbnail = img.thumbnail(512, 512).to_rgba8();
+    drop(img);
+
+    let rgb_v = color_thief::get_palette(thumbnail.as_raw(), color_thief::ColorFormat::Rgba, 5, 5)?
+        .into_iter()
+        .map(|c| models::RGB(c.r.into(), c.g.into(), c.b.into()))
+        .collect();
+    // Convert to i32 here to save to bson.
+    Ok(((w as i32, h as i32), rgb_v))
+}
+
+pub async fn retry_pager<'a, T>(
+    pager: &mut Pager<'a, T>,
+    max_tries: i32,
+) -> crate::Result<Option<T>>
+where
+    T: DeserializeOwned + pixivcrab::NextUrl,
+{
+    let mut tries = 0;
+    loop {
+        tries += 1;
+        match pager.next().await.context(error::PixivAPI) {
+            Ok(r) => {
+                return Ok(r);
+            }
+            Err(e) => {
+                if let error::Error::PixivAPI { source, .. } = &e {
+                    if let pixivcrab::error::Error::HTTP { .. } = source {
+                        if tries <= max_tries {
+                            warning!("{}", e);
+                            tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+                            continue;
+                        }
+                    }
+                }
+                return Err(e);
+            }
+        }
+    }
+}
